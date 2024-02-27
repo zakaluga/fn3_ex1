22ba703aed8da707ab1199bfa0ddc5fc104fcaa0
VRchD <listiger2@gmail.com>
2024-02-27 19:03:07 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 
-class Items {
-
+class Properties {
+private:
+    double _positionX;
+    double _positionY;
+    double _positionZ;
+    uint16_t _id; 
+public:
+    Properties(double positionX, double positionY, double positionZ, uint16_t id) {
+        _positionX = positionX;
+        _positionY = positionY;
+        _positionZ = positionZ;
+        _id = id;
+    }
+    void move(double x, double y, double z) {
+        _positionX = x;
+        _positionY = y;
+        _positionZ = z;
+    }
+};
+class Table {
+public:
+    Properties info;
+    Table(Properties p) : info(p){}
+    
+    void printInfo(){}
 };
 
 
 int main()
 {
+    Table parta(Properties(1, 1, 1, 1));
     std::cout << "Привет Мир!" << std::endl;
-
     return 0;
 }
