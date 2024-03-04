265302d502cd255c66f2c50dd046ae605a18923b
VRchD <listiger2@gmail.com>
2024-03-05 01:13:45 +0300

    reworked position class into identification class. created monitor and completed computer class

--- main.cpp
+++ main.cpp
@@ -1,38 +1,60 @@
 #include <iostream>
 
-class Position {
+
+class Identification {
 private:
     double _positionX;
     double _positionY;
     double _positionZ;
+    uint32_t _id;
 public:
-    Position(double positionX, double positionY, double positionZ): _positionX(positionX), _positionY(positionY), _positionZ(positionZ) {       
+    Identification(double positionX, double positionY, double positionZ, uint32_t id): _positionX(positionX), _positionY(positionY), _positionZ(positionZ), _id(id) {       
     }
     void move(double x, double y, double z) {
         _positionX = x;
         _positionY = y;
         _positionZ = z;
     }
+    void def(uint32_t p) {
+        _id = p;
+    }
 };
+
+
 class Table {
 public:
-    Position pos;
-    Table(Position p) : pos(p){}
+    Identification ids;
+    Table(Identification p) : ids(p){}
     
     void printInfo(){}
 };
 class Computer {
-private:
-
-
 public:
-
+    Identification ids;
+    Computer(Identification p) : ids(p){}
+   
+    void printInfo(){}
+};
+class Monitor {
+private:
+    struct Resolution {
+        uint32_t _x;
+        uint32_t _y;
+    };
+public:
+    
+    Identification ids;
+    Resolution reso;
+    Monitor(Identification p, Resolution xy) : ids(p), reso(xy){}
+    
+    void printInfo() {
+        
+    }
 };
 
 
 int main()
 {
-    Table parta(Position(1, 1, 1));
-    std::cout << "Привет Мир!" << std::endl;
+    
     return 0;
 }
