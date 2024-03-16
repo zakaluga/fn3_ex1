ea7d4d1cca01741188547b7db6a7f06a9d228175
Mas2rba <karol057569@gmail.com>
2024-03-17 02:40:43 +0300

    Кардинальная смена мысли, тотальная экономия места, ашалеть как гениально

--- main.cpp
+++ main.cpp
@@ -1,112 +1,72 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 class Parametr
 {
 public:
     double length;
     double width;
     double height;
-    const int kolvo;
+    int kolvo;
     std::string name;
-    void display()
-    {
-        std::cout << "Предмет: " << name << "\t\tКоличество: " << kolvo << " шт\t\t"
-                  << "Характеристики: " << length << " x " << width << " x " << height << "\n";
-        std::cout << "\n";
-    }
-};
-class Stul
-{
-public:
-    Parametr stulPar;
-    std::string &name = stulPar.name;
-    Stul() : stulPar({50, 100, 50, 15, "Стул "}) {}
-    Stul(double l, double w, double h, const int k, std::string n) : stulPar({w, h, l, k, n}) {}
-    void display()
-    {
-        stulPar.display();
-    };
-};
-class Parta
-{
-public:
-    Parametr partaPar;
-    std::string &name = partaPar.name;
-    Parta() : partaPar({75, 100, 250, 10, "Парта "}) {}
-    Parta(double w, double h, double l, const int k, std::string n) : partaPar({w, h, l, k, n}) {}
-    void display()
-    {
-        partaPar.display();
-    };
-};
-class PK
-{
-public:
-    const int kolvo;
-    bool pkvkl;
-    int resX;
-    int resY;
-    int diag;
-    int mem;
-    int ram;
-    std::string proc;
-    std::string vid;
-    std::string name;
-    PK() : kolvo(26), resX(3840), resY(2160), diag(27), proc("Intel core i7-14700k"), vid("NVIDIA 4060"), mem(1024), ram(32), pkvkl(false), name("ПК ") {}
-    PK(const int k, int x, int y, int d, std::string p, std::string v, int m, int r, bool vkl, std::string n) : kolvo(k), resX(x), resY(y), diag(d), proc(p), vid(v), mem(m), ram(r), pkvkl(vkl), name(n) {}
-    void display()
-    {
-        std::cout << "Предмет: " << name << "\t\tКоличество: " << kolvo << " шт\t\t"
-                  << "Разрешение экрана: " << resX << " x " << resY << "пикселей\t\t"
-                  << "\nДиагональ: " << diag << " дюймов\t\t"
-                  << "Постоянная память: " << mem << " ГБ\t\t"
-                  << "Оперативная память: " << ram << " Гб\t\t"
-                  << "\nПроцессор: " << proc << "\t\tВидеокарта: " << vid << "\n";
-        std::cout << "\n";
-    }
-};
-class Bespoleznie
-{
-public:
-    Parametr bespPar;
-    std::string name;
-    Bespoleznie(double w, double h, double l, std::string n) : bespPar({w, h, l}), name(n) {}
-    void display()
-    {
-        std::cout << "Имя предмета: " << name << std::endl;
-        std::cout << "Ширина: " << bespPar.width << " см\n";
-        std::cout << "Высота: " << bespPar.height << " см\n";
-        std::cout << "Длина: " << bespPar.length << " см\n";
-    }
-};
-class Shkaf
-{
-public:
-    Parametr shkafPar;
     std::string material;
-    std::string name;
-    Shkaf(double w, double h, double l, std::string m, std::string n) : shkafPar({w, h, l}), material(m), name(n) {}
-};
-template <typename T>
-T *makeArr(const int kolvo)
-{
-    T *klass = new T[kolvo];
-    return klass;
-    for (int i = 0; i < kolvo; i++)
+    std::string number;
+    void display()
     {
-        klass[i].name = klass[i].name + std::to_string(i + 1);
-        klass[i].display();
+        std::cout << "Предмет: " << name << "  Количество: " << kolvo << "  Размеры: "
+                  << length << " x " << width << " x " << height << "  Материал: " << material;
+        std::cout << "\n";
+    }
+    void getInf()
+    {
+        std::cout << "Количество объектов: ";
+        std::cin >> kolvo;
+        std::cout << "Материал объекта: ";
+        std::cin >> material;
+        std::cout << "Размеры: ";
+        std::cin >> length >> width >> height;
+    }
+};
+class ClassroomInv
+{
+private:
+    std::vector<Parametr> registeredItems;
+    std::vector<Parametr> unregisteredItems;
+
+public:
+    void addRegisteredItem(const Parametr &item)
+    {
+        registeredItems.push_back(item);
+    }
+
+    void addUnregisteredItem(const Parametr &item)
+    {
+        unregisteredItems.push_back(item);
     }
 };
 int main()
 {
-    Stul *stulArr = makeArr<Stul>(15);
-    Parta *partArr = makeArr<Parta>(10);
-    PK *pkArr = makeArr<PK>(26);
-    stulArr[0].display();
-    partArr[0].display();
-    pkArr[0].display();
-    delete[] stulArr;
-    delete[] partArr;
-    delete[] pkArr;
+    setlocale(LC_ALL, "Rus");
+    ClassroomInv inventory;
+    std::cout << "Стулья:\n";
+    Parametr stul;
+    stul.name = "Стул";
+    stul.getInf();
+    inventory.addRegisteredItem(stul);
+    stul.display();
+
+    Parametr parta;
+    std::cout << "Парты:\n";
+    parta.name = "Парта";
+    parta.getInf();
+    inventory.addRegisteredItem(parta);
+    parta.display();
+
+    Parametr scaf;
+    std::cout << "Шкафы:\n";
+    scaf.name = "Шкаф";
+    scaf.getInf();
+    inventory.addRegisteredItem(scaf);
+    scaf.display();
 }
