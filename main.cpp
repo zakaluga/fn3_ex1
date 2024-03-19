caa95870b32af3098e15e10d7ce7dff5a1901843
Mas2rba <karol057569@gmail.com>
2024-03-19 13:43:26 +0300

    Фул рабочая версия

--- main.cpp
+++ main.cpp
@@ -16,11 +16,14 @@ public:
     void display()
     {
         std::cout << "Item name: " << name << "  Quantity: " << kolvo << "  Size: "
-                  << length << " x " << width << " x " << height << "  Material: " << material;
+                  << length << " x " << width << " x " << height << "  Material: " << material
+                  << "  Extra information: " << about;
         std::cout << "\n";
     }
     void getInf()
     {
+        std::cout << "Enter item name: ";
+        std::cin >> name;
         std::cout << "Number of items: ";
         std::cin >> kolvo;
         if (kolvo > 0)
@@ -29,39 +32,55 @@ public:
             std::cin >> material;
             std::cout << "Size: ";
             std::cin >> length >> width >> height;
+            std::cout << "Extra information about item: ";
+            std::cin.ignore();
+            std::getline(std::cin, about);
         }
-    }
-    void getUnreg()
-    {
-        getInf();
-        std::cout << "Extra information about item: ";
-        std::cin >> about;
     }
     void addInToFile(std::ofstream &File)
     {
         File << "Item name: " << name << "  Quantity: " << kolvo << "  Size: "
-                  << length << " x " << width << " x " << height << "  Material: " << material;
+             << length << " x " << width << " x " << height << "  Material: " << material
+             << "  Extra information: " << about;
         File << "\n";
     }
 };
 class ClassroomInv
 {
 private:
+    int tempCount;
     std::vector<Parametr> registeredItems;
     std::vector<Parametr> unregisteredItems;
+
 public:
     void addRegisteredItem(const Parametr &item)
     {
         registeredItems.push_back(item);
     }
-
     void addUnregisteredItem(const Parametr &item)
     {
         unregisteredItems.push_back(item);
     }
-    void deleteItems(){
+    void deleteItems()
+    {
         registeredItems.clear();
         unregisteredItems.clear();
+    }
+    void getTempCount()
+    {
+        std::cout << "Enter number of types registred items: ";
+        std::cin >> tempCount;
+    }
+    void fillTempArr(ClassroomInv inv, std::ofstream &File)
+    {
+        Parametr *tempArr = new Parametr[tempCount];
+        for (int i = 0; i < tempCount; i++)
+        {
+            tempArr[i].getInf();
+            inv.addRegisteredItem(tempArr[i]);
+            tempArr[i].addInToFile(File);
+        }
+        delete[] tempArr;
     }
 };
 int main()
@@ -70,46 +89,21 @@ int main()
     List.clear();
     int unregTypeCount;
     ClassroomInv inventory;
+
     std::cout << "Registred items:\n";
+    inventory.getTempCount();
+    inventory.fillTempArr(inventory, List);
 
-    std::cout << "Chairs:\n";
-    Parametr stul;
-    stul.name = "Chair";
-    stul.getInf();
-    inventory.addRegisteredItem(stul);
-    stul.addInToFile(List);
-
-    Parametr parta;
-    std::cout << "Desks:\n";
-    parta.name = "Dessk";
-    parta.getInf();
-    inventory.addRegisteredItem(parta);
-    parta.addInToFile(List);
-
-    Parametr scaf;
-    std::cout << "Wardrobes:\n";
-    scaf.name = "Wardrobe";
-    scaf.getInf();
-    inventory.addRegisteredItem(scaf);
-    scaf.addInToFile(List);
-
-    Parametr board;
-    std::cout << "Boards:\n";
-    board.name = "Board";
-    board.getInf();
-    inventory.addRegisteredItem(board);
-    board.addInToFile(List);
-    
     std::cout << "How many types of unregistered items will you have? ";
     std::cin >> unregTypeCount;
 
     if (unregTypeCount > 0)
     {
         Parametr unreg;
-        Parametr unregitems[unregTypeCount];
+        Parametr *unregitems = new Parametr[unregTypeCount];
         for (int i = 0; i < unregTypeCount; i++)
         {
-            unregitems[i].getUnreg();
+            unregitems[i].getInf();
             inventory.addUnregisteredItem(unregitems[i]);
             unregitems[i].addInToFile(List);
         }
