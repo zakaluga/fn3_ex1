43a22c2cd9e33e4d2b9154a5fb509f11ded3e43a
papr1ka <145461527+12347372773@users.noreply.github.com>
2024-03-26 19:02:32 +0300

    Add files via upload

---
+++ lesson6_Dubinin.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <string>
+#include <fstream>
+
+
+
+
+struct Base{
+    int width;
+    int length;
+    std::string color;
+};
+
+void Print_name (std::string name )
+{
+    std::cout<<""<<name<<std::endl;
+};
+
+void Print_digit (int digit)
+{
+    std::cout<<"tjdtyj"<<digit<<std::endl;
+};
+
+
+class Computers {
+    Base computer;
+    int number;
+
+    public:
+    void Set_number(){
+
+    };
+    void Get_number(){
+        Print_digit(number);
+        
+    };
+
+  
+};
+class Monitors{
+    Base screen;
+    int resolution;
+    
+
+
+};
+
+class Portaits{
+public:
+    Base picture;
+    
+};
+
+class Tables{
+
+};
+
+
+
+int main(){
+    using namespace std;
+
+    /*ofstream myfile;
+    myfile.open("inventar.txt");
+    myfile.
+*/
+
+cout<<"Андрей пидар"<<endl;
+};
+
