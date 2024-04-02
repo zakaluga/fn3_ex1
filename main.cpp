a86a29a6a96e89bf7ac55b323fb1922af6103de2
Mishail Panow <misha.p2005@mail.ru>
2024-04-02 11:17:37 +0300

    Доделал все сетеры и гетеры, доделал везде edit(). Не могу реализовать мапу вариантов.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <variant>
+#include <map>
 
 class Processor {
     private:
@@ -37,6 +39,7 @@ class Processor {
             if (this->isWork_) {std::cout << "Processor is OK\n";} 
             else               {std::cout << "Processor is NOT OK\n";}
         }
+
         void edit() {
             int  comandNumber;
             char yesOrNo;
@@ -79,13 +82,8 @@ class Processor {
             case 5:
                 std::cout << "Is the processor still working? (Y or N): ";
                 std::cin  >> yesOrNo;
-                if ((yesOrNo == 'Y') || (yesOrNo == 'y')) {
-                    std::cout << "Processor is still working\n";
-                    this->isWork_ = true;
-                } else if ((yesOrNo == 'n') || (yesOrNo == 'N')) {
-                    std::cout << "Processor doesn't working. Repare it\n";
-                    this->isWork_ = false;
-                }
+                if      ((yesOrNo == 'Y') || (yesOrNo == 'y')) {std::cout << "Processor is still working\n";          this->isWork_ = true; } 
+                else if ((yesOrNo == 'n') || (yesOrNo == 'N')) {std::cout << "Processor doesn't working. Repare it\n";this->isWork_ = false;}
                 this->edit();
                 break;
             case 6:
@@ -163,8 +161,8 @@ class GraphCard {
             case 4:
                 std::cout << "Is the graphcard still working?(Y or N): ";
                 std::cin  >> yesOrNo;
-                if ((yesOrNo == 'n') || (yesOrNo == 'N')) {std::cout << "Graph card doesn't work. Repare it\n"; this->isWork_ = false;} 
-                else if ((yesOrNo == 'y') || (yesOrNo == 'Y')) {std::cout << "Graph card is still working.\n"; this->isWork_ = true;}
+                if      ((yesOrNo == 'n') || (yesOrNo == 'N')) {std::cout << "Graph card doesn't work. Repare it\n"; this->isWork_ = false;} 
+                else if ((yesOrNo == 'y') || (yesOrNo == 'Y')) {std::cout << "Graph card is still working.\n";       this->isWork_ = true; }
                 this->edit();
                 break;
             case 5:
@@ -204,7 +202,6 @@ class Monitor {
             if (this->onOff_)  {std::cout << "Monitor is ON";} else {std::cout << "Monitor is OFF";}
             if (this->isWork_) {std::cout << "Monitor is OK";} else {std::cout << "Monitor is NOT OK";}
         }
-        void PushButton() {this->onOff_ = !this->onOff_;}
 
         void edit() {
             int comandNumber;
@@ -236,7 +233,7 @@ class Monitor {
                 if (this->isWork_) {
                     std::cout << "What you want to do: ON or OFF (Y or N)";
                     std::cin  >> yesOrNo;
-                    if ((yesOrNo == 'y') || (yesOrNo == 'Y')) {this->onOff_ = true; std::cout << "You ON monitor\n";} 
+                    if      ((yesOrNo == 'y') || (yesOrNo == 'Y')) {this->onOff_ = true;  std::cout << "You ON monitor\n"; } 
                     else if ((yesOrNo == 'n') || (yesOrNo == 'N')) {this->onOff_ = false; std::cout << "You OFF monitor\n";}
                 } else {
                     std::cout << "Repare monitor befor work with them\n";
@@ -246,8 +243,8 @@ class Monitor {
             case 4:
                 std::cout << "Is the monitor still working?(Y or N): ";
                 std::cin  >> yesOrNo;
-                if ((yesOrNo == 'n') || (yesOrNo =='N')) {std::cout << "Monitor doesn't work. Repare it\n"; this->isWork_ = false;} 
-                else if ((yesOrNo == 'y') || (yesOrNo == 'Y')) {std::cout << "Monitor is still working.\n"; this->isWork_ = true;}
+                if      ((yesOrNo == 'n') || (yesOrNo == 'N')) {std::cout << "Monitor doesn't work. Repare it\n"; this->isWork_ = false;} 
+                else if ((yesOrNo == 'y') || (yesOrNo == 'Y')) {std::cout << "Monitor is still working.\n";       this->isWork_ = true; }
                 this->edit();
                 break;
             case 5:
@@ -277,25 +274,27 @@ class Computer {
         Computer() = default;
         ~Computer() = default;
 
-        Monitor& getMonitor()              {return this->monitor_;}
-        void setMonitor(Monitor& screen)   {this->monitor_ = screen;}
+        Monitor& getMonitor()                   {return this->monitor_;}
+        void setMonitor(Monitor& monitor)       {this->monitor_ = monitor;}
 
-        GraphCard& getGraphCard()          {return this->graphCard_;}
-        void setGraphCard(GraphCard& card) {this->graphCard_ = card;}
+        GraphCard& getGraphCard()               {return this->graphCard_;}
+        void setGraphCard(GraphCard& graphCard) {this->graphCard_ = graphCard;}
 
-        Processor& getProcessor()          {return this->processor_;}
-        void setProcessor(Processor& proc) {this->processor_ = proc;}
+        Processor& getProcessor()               {return this->processor_;}
+        void setProcessor(Processor& processor) {this->processor_ = processor;}
 
-        bool getIsWork()                   {return this->isWork_;}
-        void setIsWork(bool work)          {this->isWork_ = work;}
+        bool getIsWork()                        {return this->isWork_;}
+        void setIsWork(bool isWork)             {this->isWork_ = isWork;}
 
         void getInfo() {
+            std::cout << "COMPUTER\n\n";
             this->monitor_.getInfo();
             this->graphCard_.getInfo();
             this->processor_.getInfo();
             if (this->onOff_)  {std::cout << "Computer is ON\n";} else {std::cout << "Computer is OFF\n";}
             if (this->isWork_) {std::cout << "Computer is OK\n";} else {std::cout << "Something is wrong, check components and fix them";}
         }
+
         void edit() {
             int comandNumber;
             char yesOrNo;
@@ -326,8 +325,8 @@ class Computer {
                 if (this->isWork_) {
                     std::cout << "ON or Off (Y or N)?: ";
                     std::cin >> yesOrNo;
-                    if ((yesOrNo = 'y') || (yesOrNo = 'Y')) {this->onOff_ = true; std::cout << "Computer is ON\n";}
-                    else if ((yesOrNo = 'n') || (yesOrNo = 'N')) {this->onOff_ = false; std::cout << "Computer is OFF\n";}
+                    if      ((yesOrNo == 'y') || (yesOrNo == 'Y')) {this->onOff_ = true;  std::cout << "Computer is ON\n";}
+                    else if ((yesOrNo == 'n') || (yesOrNo == 'N')) {this->onOff_ = false; std::cout << "Computer is OFF\n";}
                 } else {std::cout << "Repare computer before work with them\n";}
                 break;
             case 5:
@@ -344,7 +343,64 @@ struct Sizes {
     private:
         int a_, b_, c_;
     public:
-        Sizes(int a, int b, int c): a_(a), b_(b), c_(c) {}
+        Sizes(int a, int b, int c): a_(a), b_(b), c_(c) {};
+        Sizes()  = default;
+        ~Sizes() = default;
+
+        int  getA()      {return this->a_;}
+        void setA(int a) {this->a_ = a;}
+
+        int  getB()      {return this->b_;}
+        void setB(int b) {this->b_ = b;}
+        
+        int  getC()      {return this->c_;}
+        void setC(int c) {this->c_ = c;}
+
+        void getInfo() {
+            std::cout << "SIZES\n\n";
+            std::cout << "Size a: " << this->a_
+                      << "Size b: " << this->b_
+                      << "Size c: " << this->c_;
+        }
+
+        void edit() {
+            int commandNumber;
+            std::cout << "---SIZES MENU---\n\n";
+            std::cout << "What you want to edit?\n\t" 
+                      << "1) Size a\n\t"
+                      << "2) Size b\n\t"
+                      << "3) Size c\n\t" 
+                      << "4) Exit\nPress any key (1-3): ";
+            std::cin  >> commandNumber;
+            switch (commandNumber)
+            {
+            case 1:
+                std::cout << "Enret new size a: ";
+                std::cin  >> this->a_;
+                std::cout << "New size a: "+this->a_;
+                this->edit();
+                break;
+            case 2:
+                std::cout << "Enret new size b: ";
+                std::cin  >> this->b_;
+                std::cout << "New size b: "+this->b_;
+                this->edit();
+                break;
+            case 3:
+                std::cout << "Enret new size c: ";
+                std::cin  >> this->c_;
+                std::cout << "New size c: "+this->c_;
+                this->edit();
+                break;
+            case 4:
+                this->getInfo();
+                break;
+                    
+            default:
+                this->edit();
+                break;
+            }
+        }
 };
 
 struct Position {
@@ -361,7 +417,44 @@ struct Position {
         void setY(int& y) {this->y_ = y;}
         int getY()        {return this->y_;}
 
-        void set(int pos_x, int pos_y) {this->x_ = pos_x; this->y_ = pos_y;}
+        void getInfo() {
+            std::cout << "POSITION\n\n";
+            std::cout << "x: " << this->x_
+                      << "y: " << this->y_;
+        }
+
+        void edit() {
+            int commandNumber;
+            std::cout << "What you want to change?\n";
+            std::cout << "What you want to change?\n\t"
+                      << "1) Coordinate x\n\t"
+                      << "2) Coordinate y\n\t"
+                      << "3) Exit\n"
+                      << "Press any key(1-3)";
+            std::cin  >> commandNumber;
+            switch (commandNumber)
+            {
+            case 1:
+                std::cout << "Enter new coordinate x: ";
+                std::cin  >> this->x_;
+                std::cout << "New coordinate x: " + this->x_;
+                this->edit();
+                break;
+            case 2:
+                std::cout << "Enter new coordinate y: ";
+                std::cin  >> this->y_;
+                std::cout << "New coordinate y: " + this->y_;
+                this->edit();
+                break;
+            case 3:
+                this->getInfo();
+                break;
+            default:
+                this->edit();
+                break;
+            }
+        }
+        
 };
 
 class FurnetureProperties {
@@ -371,59 +464,154 @@ class FurnetureProperties {
         MATERIAL_PLASTIC, 
         MATERIAL_MIXED
     };
-    std::string name_;
     Sizes       sizes_;
     Material    material_;
     Position    position_;
 public:
-    FurnetureProperties(std::string& name, Material& mat, const Sizes& s, Position& pos) :name_(name), sizes_(s), material_(mat), position_(pos) {};
+    FurnetureProperties(Material& material, const Sizes& sizes, Position& position) :sizes_(sizes), material_(material), position_(position) {};
     FurnetureProperties() = default;
     ~FurnetureProperties() = default;
 
-    std::string GetFurnetureName()              {return this->name_;}
-    void SetFurnetureName(std::string& name)    {this->name_ = name;}
+    Sizes getsizes()                     {return this->sizes_;}
+    void setSizes(Sizes& sizes)          {this->sizes_ = sizes;}
 
-    Sizes GetFurnetureSizes()                   {return this->sizes_;}
-    void SetFurnetureSizes(Sizes& size)         {this->sizes_ = size;}
-
-    Material GetFurnetureMaterial()             {return this->material_;}
-    void SetFurnetureMaterial(Material& mater)  {this->material_ = mater;}
+    Material getMaterial()               {return this->material_;}
+    void setMaterial(Material& material) {this->material_ = material;}
  
-    Position GetFurneturePosition()             {return this->position_;}
-    void SetFurneturePosition(Position& pos)    {this->position_ = pos;}
+    Position getPosition()               {return this->position_;}
+    void setPosition(Position& position) {this->position_ = position;}
 
-    void Move(int move_x, int move_y) {
-        std::cout << "Enter new coordinates: ";
-        std::cin >> move_x >> move_y;
-        this->position_.set(move_x, move_y);
-    };
+    void edit() {
+        int comandNumber;
+        std::cout << "---FURNETURE MENU---";
+        std::cout << "What you want to change?\n\t"
+                  << "1) Material\n\t"
+                  << "2) Sizes\n\t"
+                  << "3) Position\n\t"
+                  << "4) Exit\n"
+                  << "Press any key(1-4): ";
+        std::cin  >> comandNumber;
+        switch (comandNumber)
+        {
+        case 1:
+            int materialNumber;
+            std::cout << "What material you want to choose?\n\t"
+                      << "1) Wood\n\t"
+                      << "2) Steel\n\t"
+                      << "3) Plastic\n\t"
+                      << "4) Mixed\n\t"
+                      << "5) Exit\n\t"
+                      << "Press any key(1-5): ";
+            std::cin  >> materialNumber;
+            switch (materialNumber)
+            {
+            case 1:
+                std::cout << "New material: wood";
+                this->material_ = MATERIAL_WOOD;
+                this->edit();
+                break;
+            case 2:
+                std::cout << "New material: steel";
+                this->material_ = MATERIAL_STEEL;
+                this->edit();
+                break;
+            case 3:
+                std::cout << "New material: plastic";
+                this->material_ = MATERIAL_PLASTIC;
+                this->edit();
+                break;
+            case 4:
+                std::cout << "New material: mixed";
+                this->material_ = MATERIAL_MIXED;
+                this->edit();
+                break;
+            case 5:
+                break;
+            default:
+                this->edit();
+                break;
+            }
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+        }
+    }
 };
 
 class Chair {
 public:
     FurnetureProperties furnetureProperties_;
+
     Chair(const FurnetureProperties& furnetureProperties) :furnetureProperties_(furnetureProperties) {};
+    Chair() = default;
+    ~Chair() = default;
 };
 
 class Locker {
 public:
     FurnetureProperties furnetureProperties_;
+
     Locker(const FurnetureProperties& furnetureProperties) :furnetureProperties_(furnetureProperties) {};
+    Locker() = default;
+    ~Locker() = default;
 };
 
 class Table {
 public:
     FurnetureProperties furnetureProperties_;
+
     Table(const FurnetureProperties& furnetureProperties) :furnetureProperties_(furnetureProperties) {};
+    Table() = default;
+    ~Table() = default;
 };
+
+std::map<std::string, std::variant<Computer, Monitor, FurnetureProperties>> inventory;
+
+struct autoObjectMenu {
+    void operator()(Computer& object) {object.edit();}
+};
+
+void Menu() {
+    int commandNumber;
+    std::cout << "---MAIN MENU---\n";
+    std::cout << "What you want to do?\n\t"
+              << "1) View registred objects\n\t"
+              << "2) Add new objects\n\t"
+              << "3) Edit objects\n\t"
+              << "4) Delete object\n\t"
+              << "5) Work with file\n\t"
+              << "6) Exit\n"
+              << "Press any key (1-6): ";
+    std::cin >> commandNumber;
+    switch (commandNumber)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    
+    default:
+        break;
+    } 
+}
 
 int main()
 {
-    Processor *proc = new Processor();
-    proc->edit();
-    GraphCard *card = new GraphCard();
-    card->edit();
-    Monitor *screen = new Monitor();
-    screen->edit();
+    Computer *computer = new Computer();
+    inventory["a228"] = std::variant<Computer, Monitor, FurnetureProperties>(*computer);
     return 0;
 }
