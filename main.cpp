046cdbcac8dc3bf24515b51dd8de42d446ae9a4b
Mas2rbek <karol057569@gmail.com>
2024-04-03 11:33:25 +0300

    333

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+﻿#include <algorithm>
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,8 +8,8 @@ class AggregatedSettings
 {
 public:
     int count;
-    std::string size;
     std::string name;
+    std::string size;
     std::string material;
     std::string extraInf;
     std::map<std::string, std::string> func;
@@ -17,6 +18,11 @@ public:
         std::cout << "Item name: " << name << " | Number: " << count << " | Size: "
                   << size << " | Material: " << material
                   << " | Extra information: " << extraInf;
+        std::cout << "\n";
+    }
+    void noteddisplay()
+    {
+        std::cout << "Item name: " << name << " | Number: " << count << " | Extra information: " << extraInf;
         std::cout << "\n";
     }
     void getinf()
@@ -28,13 +34,23 @@ public:
         if (count > 0)
         {
             std::cout << "Material of item: ";
-            std::cin >> material;
-            std::cout << "Size: ";
             std::cin.ignore();
+            std::getline(std::cin, material);
+            std::cout << "Size: ";
             std::getline(std::cin, size);
             std::cout << "Extra information extraInf item: ";
             std::getline(std::cin, extraInf);
         }
+    }
+    void getnotedinf()
+    {
+        std::cout << "Enter item name: ";
+        std::cin >> name;
+        std::cout << "Number of items: ";
+        std::cin >> count;
+        std::cout << "Enter information: ";
+        std::cin.ignore();
+        std::getline(std::cin, extraInf);
     }
     void addInToFile(std::ofstream &File)
     {
@@ -51,6 +67,14 @@ public:
         File << material << "\n";
         File << extraInf << "\n";
     }
+    void clear()
+    {
+        count = 0;
+        name = "";
+        size = "";
+        material = "";
+        extraInf = "";
+    }
 };
 class ClassroomInventory
 {
@@ -60,10 +84,6 @@ public:
     void addItems(const AggregatedSettings &item)
     {
         items.push_back(item);
-    }
-    void deleteItems()
-    {
-        items.clear();
     }
     void addInToFile(std::ofstream &file, std::fstream &tempfile)
     {
@@ -98,86 +118,140 @@ void scanfile(std::fstream &file, std::map<std::string, AggregatedSettings> &map
         }
     }
 }
+bool findItem(std::vector<std::string> vect, std::string name)
+{
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if (name == vect[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
 
 void Menu(ClassroomInventory &inventory)
 {
     int choice;
-    int i = 0;
     std::vector<std::string> gg;
     std::map<std::string, AggregatedSettings> items;
     std::ofstream file("Result.txt");
-    std::fstream tempfile("DON'T_CLICK_HERE", std::ios::out | std::ios::in);
+    std::fstream tempfile("DON'T_CLICK_HERE.txt", std::ios::out | std::ios::in);
     AggregatedSettings temp;
     std::string choosenName;
+    std::string deletename;
     scanfile(tempfile, items, gg);
     tempfile.close();
-    tempfile.open("DON'T_CLICK_HERE", std::ios::out | std::ios::in | std::ios::trunc);
-    while (choice != 5)
+    tempfile.open("DON'T_CLICK_HERE.txt", std::ios::out | std::ios::in | std::ios::trunc);
+    while (choice != 7)
     {
         int choiseForCase3;
         std::cout << "Menu:\n";
-        std::cout << "1) Add standart item.\n";
-        std::cout << "2) Add unusual item.\n";
+        std::cout << "1) Add study item.\n";
+        std::cout << "2) Add not educational item.\n";
         std::cout << "3) Change setting of reg item.\n";
-        std::cout << "4) Clean file.\n";
-        std::cout << "5) Exit.\n";
+        std::cout << "4) Delete all items.\n";
+        std::cout << "5) Delete choosen item\n";
+        std::cout << "6) Display all items\n";
+        std::cout << "7) Exit.\n";
         std::cin >> choice;
         switch (choice)
         {
         case 1:
             temp.getinf();
-            items[temp.name] = temp;
-            gg.push_back(temp.name);
-            i++;
+            if (temp.count > 0)
+            {
+                items[temp.name] = temp;
+                gg.push_back(temp.name);
+                temp.clear();
+            }
             break;
         case 2:
-            temp.getinf();
-            items[temp.name] = temp;
-            gg.push_back(temp.name);
-            i++;
+            temp.getnotedinf();
+            if (temp.count > 0)
+            {
+                items[temp.name] = temp;
+                gg.push_back(temp.name);
+                temp.clear();
+            }
             break;
         case 3:
             std::cout << "Which item will be changed: ";
             std::cin >> choosenName;
-            std::cout << "Which setting you want to change?\n";
-            std::cout << "1) Number of items\n";
-            std::cout << "2) Size\n";
-            std::cout << "3) Material\n";
-            std::cout << "4) Extra information\n";
-            std::cout << "5) Exit\n";
-            std::cin >> choiseForCase3;
-            switch (choiseForCase3)
+            if (findItem(gg, choosenName))
             {
-            case 1:
-                std::cout << "Enter new count: ";
-                std::cin >> items[choosenName].count;
-                break;
-            case 2:
-                std::cout << "Enter new size: ";
-                std::cin >> items[choosenName].size;
-                break;
-            case 3:
-                std::cout << "Enter new material: ";
-                std::cin >> items[choosenName].material;
-                break;
-            case 4:
-                std::cout << "Enter new extra information: ";
-                std::cin.ignore();
-                std::getline(std::cin, items[choosenName].extraInf);
-                break;
-            case 5:
-                break;
+                std::cout << "Which setting you want to change?\n";
+                std::cout << "1) Number of items\n";
+                std::cout << "2) Size\n";
+                std::cout << "3) Material\n";
+                std::cout << "4) Extra information\n";
+                std::cout << "5) Exit\n";
+                std::cin >> choiseForCase3;
+                switch (choiseForCase3)
+                {
+                case 1:
+                    std::cout << "Enter new count: ";
+                    std::cin >> items[choosenName].count;
+                    break;
+                case 2:
+                    std::cout << "Enter new size: ";
+                    std::cin.ignore();
+                    std::getline(std::cin, items[choosenName].size);
+                    break;
+                case 3:
+                    std::cout << "Enter new material: ";
+                    std::cin.ignore();
+                    std::getline(std::cin, items[choosenName].material);
+                    break;
+                case 4:
+                    std::cout << "Enter new extra information: ";
+                    std::cin.ignore();
+                    std::getline(std::cin, items[choosenName].extraInf);
+                    break;
+                case 5:
+                    break;
 
-            default:
-                std::cout << "No match!";
+                default:
+                    std::cout << "No match!\n";
+                    break;
+                }
                 break;
             }
-            break;
+            else
+            {
+                std::cout << "Wrong name\n";
+            }
 
         case 4:
             file.clear();
+            tempfile.clear();
+            items.clear();
             break;
         case 5:
+            std::cout << "Which item will be deleted?: ";
+            std::cin >> deletename;
+            if (findItem(gg, deletename))
+            {
+                items[deletename].clear();
+                for (auto it = gg.begin(); it != gg.end(); ++it)
+                {
+                    if (*it == deletename)
+                    {
+                        gg.erase(it);
+                        break;
+                    }
+                }
+            }
+            break;
+        case 6:
+            for (int i = 0; i < gg.size(); i++)
+            {
+                items[gg[i]].display();
+            }
+            break;
+
+        case 7:
             for (int i = 0; i < gg.size(); i++)
             {
                 inventory.addItems(items[gg[i]]);
@@ -188,7 +262,7 @@ void Menu(ClassroomInventory &inventory)
             break;
 
         default:
-            std::cout << "No match!";
+            std::cout << "No match!\n";
             break;
         }
     }
