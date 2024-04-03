fb5575f1591c6fb902fa471529d8b8c6a45a9e64
VRchD <listiger2@gmail.com>
2024-04-03 11:50:28 +0300

    ААААА Я ВСЁ СЛОМАЛ

--- main.cpp
+++ main.cpp
@@ -115,13 +115,13 @@
       ~Unregistered() = default;
   };
 //all classes block end
-
 //general block ncurses windows start
   int maxY = 0;
   int maxX = 0;
-
+  WINDOW *wmain;
   void init() {
     initscr(); // создание окна
+    clear();
     keypad(stdscr, TRUE);
     cbreak();
     getmaxyx(stdscr, maxY, maxX);
@@ -132,20 +132,50 @@
     refresh();
   }
 //general block ncurses windows end
+  void escape(WINDOW* w) {
+    delwin(w);
+    clear();
+    endwin();
+  }
+  
+  void create(std::vector<std::string> list, char item[30], int x, int y, int n) {
+    init();
+    item[17];
+    int i = 0;
+    wmain = newwin(y, x, (maxY-y)/2, (maxX-x)/2); // create a new window
+    box(wmain, 0, 0); // sets default borders for the window
+    // now print all the menu items and highlight the first one
+    wattron(wmain, A_STANDOUT);
+    sprintf(item, "%s",  list[i].c_str());
+    mvwprintw( wmain, i+1, 2, "%s", item );
+    for(i = 1; i <= n; ++i) {
+      wattroff(wmain, A_STANDOUT);
+      sprintf(item, "%s",  list[i].c_str());
+      mvwprintw( wmain, i+1, 2, "%s", item );
+    }
+    wrefresh(wmain); // update the terminal screen
+    i = 0;
+    noecho(); // disable echoing of characters on the screen
+    keypad(wmain, TRUE ); // enable keyboard input for the window.
+    curs_set( 0 ); // hide the default screen cursor.
+  }
 
 
   void window2() {
     WINDOW *w;
     init();
-    std::vector<std::string> list = {"Add new furniture", "Add new book", "Add new computer", "Add new monitor", "Add unregistered object"};
-    char item[30];
+    std::vector<std::string> list = {"Add new furniture", "Add new book", "Add new computer", "Add new monitor", "Add unregistered object", "Get Back"};
+    char item[24];
     int ch;
     int i = 0;
     
-    w = newwin(10, 20, 1, 1 ); // create a new window
+    w = newwin(10, 24, (maxY-10)/2, (maxX-24)/2); // create a new window
     box(w, 0, 0); // sets default borders for the window
     // now print all the menu items and highlight the first one
-    for(i = 0; i < 4; ++i) {
+    wattron(w, A_STANDOUT);
+    sprintf(item, "%s",  list[i].c_str());
+    mvwprintw( w, i+1, 2, "%s", item );
+    for(i = 1; i <= 5; ++i) {
       wattroff(w, A_STANDOUT);
       sprintf(item, "%s",  list[i].c_str());
       mvwprintw( w, i+1, 2, "%s", item );
@@ -155,58 +185,8 @@
     noecho(); // disable echoing of characters on the screen
     keypad(w, TRUE ); // enable keyboard input for the window.
     curs_set( 0 ); // hide the default screen cursor.
-    // get the input
-    while((ch = wgetch(w)) != 'q') { 
-      // right pad with spaces to make the items appear with even width.
-      sprintf(item, "%s",  list[i].c_str()); 
-      mvwprintw( w, i+1, 2, "%s", item ); 
-      // use a variable to increment or decrement the value based on the input.
-      switch(ch) {
-        case (int)'\n':
-          delwin(w);
-          clear();
-          endwin();
-        case KEY_UP:
-          --i;
-          i = (i < 0) ? 4 : i;
-          break;
-        case KEY_DOWN:
-          ++i;
-          i = (i > 4) ? 0 : i;
-          break;
-      }
-      // now highlight the next item in the list.
-      wattron(w, A_STANDOUT);
-      sprintf(item, "%s",  list[i].c_str());
-      mvwprintw( w, i+1, 2, "%s", item);
-      wattroff(w, A_STANDOUT);
-    }
-    delwin(w);
-    clear();
-    endwin();
-  }
-//first window block start
-  void window1() {
-    WINDOW *w;
-    init();
-    std::vector<std::string> list = {"Create object", "Read object info", "Delete object", "Exit"};
-    char item[30];
-    int ch;
-    int i = 0;
     
-    w = newwin(10, 20, 1, 1 ); // create a new window
-    box(w, 0, 0); // sets default borders for the window
-    // now print all the menu items and highlight the first one
-    for(i = 0; i < 3; ++i) {
-      wattroff(w, A_STANDOUT);
-      sprintf(item, "%s",  list[i].c_str());
-      mvwprintw( w, i+1, 2, "%s", item );
-    }
-    wrefresh(w); // update the terminal screen
-    i = 0;
-    noecho(); // disable echoing of characters on the screen
-    keypad(w, TRUE ); // enable keyboard input for the window.
-    curs_set( 0 ); // hide the default screen cursor.
+    
     // get the input
     while((ch = wgetch(w)) != 'q') { 
       // right pad with spaces to make the items appear with even width.
@@ -215,21 +195,32 @@
       // use a variable to increment or decrement the value based on the input.
       switch(ch) {
         case (int)'\n':
-          delwin(w);
+          initscr();
           clear();
+          delwin(w);
           switch(i) {
-            case 0:
-                window2();
-            case 1: 
-                
+            case 0: //add new furniture
+              escape(w);
+            case 1: //
+              escape(w);
+            case 2: //
+              escape(w);
+            case 3:
+              escape(w);
+            case 4:
+              escape(w);
+            case 5:
+              escape(w);
+              //window1();
+              
           }
         case KEY_UP:
           --i;
-          i = (i < 0) ? 3 : i;
+          i = ((i < 0) ? 5 : i);
           break;
         case KEY_DOWN:
           ++i;
-          i = (i > 3) ? 0 : i;
+          i = (i > 5) ? 0 : i;
           break;
       }
       // now highlight the next item in the list.
@@ -238,9 +229,56 @@
       mvwprintw( w, i+1, 2, "%s", item);
       wattroff(w, A_STANDOUT);
     }
-    delwin(w);
-    clear();
-    endwin();
+    escape(w);
+    exit;
+  }
+  //first window block start
+  void window1() {
+    int ch;
+    char item[30];
+    int x = 20;
+    int y = 10;
+    int n = 3;
+    std::vector<std::string> list = {"Create object", "Read object info", "Delete object", "Exit"};
+    create(list, item, x, y, n);   
+    // get the input
+    int i = 0;
+    while((ch = wgetch(wmain)) != 'q') { 
+      sprintf(item, "%s",  list[i].c_str()); 
+      mvwprintw( wmain, i+1, 2, "%s", item ); 
+      // use a variable to increment or decrement the value based on the input.
+      switch(ch) {
+        case (int)'\n':
+          initscr();
+          clear();
+          switch(i) {
+            case 0:
+              window2();
+            case 1: 
+              window2();
+            case 2:
+              window2();
+            case 3:
+              escape(wmain);
+              exit;
+          }
+        case KEY_UP:
+          --i;
+          i = ((i < 0) ? n : i);
+          break;
+        case KEY_DOWN:
+          ++i;
+          i = (i > n) ? 0 : i;
+          break;
+      }
+      // now highlight the next item in the list.
+      wattron(wmain, A_STANDOUT);
+      sprintf(item, "%s",  list[i].c_str()); 
+      mvwprintw( wmain, i+1, 2, "%s", item ); 
+      wattroff(wmain, A_STANDOUT);
+    }
+    escape(wmain);
+    exit;
   }
 //first window block end
 int main() {
