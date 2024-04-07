69543f33f2806eeb6572b564376d5e2b8fabdd85
VRchD <listiger2@gmail.com>
2024-04-07 16:58:29 +0300

    Я ненавижу C++

--- main.cpp
+++ main.cpp
@@ -19,18 +19,20 @@
     getmaxyx(stdscr, maxY, maxX);
     clear();
   }
-  void menu_item_delete(MENU* selected_menu, ITEM** selected_items, int arrlength, WINDOW* main_window) {
+  void menu_item_delete(MENU* selected_menu, ITEM** selected_items, int arrlength, WINDOW* selected_window) {
     unpost_menu(selected_menu);
     free_menu(selected_menu);
     for(int i=0; i <= arrlength; ++i) {
       free_item(selected_items[i]);
     }
-    wrefresh(main_window);
+    delwin(selected_window);
+    wrefresh(selected_window);
   }
-  int menu_window(const char* arr[], int arrlength, WINDOW* main_window, MENU* selected_menu, ITEM** selected_items) { 
+  int menu_window(const char* arr[], int arrlength, MENU* selected_menu, ITEM** selected_items) { 
+    WINDOW *selected_window;
     int ch;
-    int sizeX = 20;
-    int sizeY = 10;
+    int sizeX = 25;
+    int sizeY = 15;
     selected_items = (ITEM**) malloc(arrlength+1);
     for(int i=0; i<arrlength; i++ ) {
       selected_items[i] = new_item(arr[i], "");
@@ -39,21 +41,21 @@
     /* Create Menu */
     selected_menu = new_menu(selected_items);
     /* Create Window for the menu */
-    main_window = newwin(sizeY, sizeX, (maxY-sizeY)/3, (maxX-sizeX)/3);
-    box( main_window, 0, 0 );
-    keypad(main_window, TRUE);
+    selected_window = newwin(sizeY, sizeX, (maxY-sizeY)/3, (maxX-sizeX)/3);
+    box( selected_window, 0, 0 );
+    keypad(selected_window, TRUE);
     /* set menu window */
-    set_menu_win(selected_menu, main_window);
+    set_menu_win(selected_menu, selected_window);
     /*set menu sub window */
-    set_menu_sub(selected_menu, derwin(main_window, sizeY-(sizeY/5), sizeX-(sizeX/5), sizeY/5, sizeX/5));
+    set_menu_sub(selected_menu, derwin(selected_window, sizeY-(sizeY/5), sizeX-(sizeX/5), sizeY/5, sizeX/5));
     set_menu_mark(selected_menu, " "); /* string used as menu marker */
     /* set menu format - no of items to be displayed */
     set_menu_format(selected_menu, 5, 1);
     /*post the menu */
     post_menu(selected_menu);
-    wrefresh(main_window);
+    wrefresh(selected_window);
     int i = 0;
-    while((ch = wgetch(main_window))!= 23)
+    while((ch = wgetch(selected_window))!= 23)
     {
       switch(ch) {
         case KEY_DOWN:
@@ -83,7 +85,7 @@
               break;
             }
       }
-      wrefresh(main_window);
+      wrefresh(selected_window);
     }
     return 0;
   }
@@ -93,8 +95,8 @@
   int menu_window() {      
         const char *main_menu_choices[] = {"Append item", "Get info", "Delete item", "Exit"}, 
           *append_menu_choices[] = {"Add Book", "Add Furniture", "Add Computer", "Add Monitor", "Add Other"}, 
-          *ginfo_menu_choices[] = {"Get all objects info", "Get books info", "Get furniture info", "Get computers info", "Get monitors info", "Get other info"}, 
-          *del_menu_choices[] = {"Delete all objects", "Delete book", "Delete furniture", "Delete computer", "Delete monitor", "Delete other"};
+          *ginfo_menu_choices[] = {"Get all info", "Get books info", "Get furniture info", "Get computers info", "Get monitors info", "Get other info"}, 
+          *del_menu_choices[] = {"Delete all", "Delete book", "Delete furniture", "Delete computer", "Delete monitor", "Delete other"};
         MENU 
           *main_menu, *append_menu, *ginfo_menu, *del_menu;
         ITEM 
@@ -104,7 +106,7 @@
     /* Create Items */
     int arrlength = 4;
     int ch;
-    int sizeX = 20;
+    int sizeX = 25;
     int sizeY = 10;
     main_menu_items = (ITEM**) malloc(arrlength+1);
     for(int i=0; i<arrlength; i++ ) {
@@ -145,15 +147,20 @@
           break;
         case '\n':
             if (current_item(main_menu) == main_menu_items[0]) {
-              menu_window(append_menu_choices, 5, main_window, append_menu, append_menu_items);
+              menu_item_delete(main_menu, main_menu_items, 4, main_window);            
+              menu_window(append_menu_choices, 5, append_menu, append_menu_items);
               break;
             }
             else if (current_item(main_menu) == main_menu_items[1]) {
-              menu_window(ginfo_menu_choices, 6, main_window, ginfo_menu, ginfo_menu_items);
+              menu_item_delete(main_menu, main_menu_items, 4, main_window);
+              
+              menu_window(ginfo_menu_choices, 6, ginfo_menu, ginfo_menu_items);
               break;
             }
             else if (current_item(main_menu) == main_menu_items[2]) {
-              menu_window(del_menu_choices, 6, main_window, del_menu, del_menu_items);
+              menu_item_delete(main_menu, main_menu_items, 4, main_window);
+              
+              menu_window(del_menu_choices, 6, del_menu, del_menu_items);
               break;
             }
             else if (current_item(main_menu) == main_menu_items[3]) {
