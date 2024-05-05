cdd406b040efbf5b58b9f10d24b932945dca42ca
VRchD <listiger2@gmail.com>
2024-05-05 19:37:23 +0300

    Наконец-то программа готова

--- main.cpp
+++ main.cpp
@@ -172,7 +172,7 @@ int a2i(const char *s)
     {"Add Book", "Add Furniture", "Add Computer",  "Add Other", "Back"};
   const std::vector<std::string> ginfo_choices = {"Get all info", "Get books info", "Get furniture info", 
     "Get computers info", "Get other info", "Back"};
-  const std::vector<std::string> del_choices = {"Delete all", "Delete book", "Delete furniture", "Delete computer", 
+  const std::vector<std::string> del_choices = {"Delete book", "Delete furniture", "Delete computer", 
     "Delete other", "Back"};
   // Window and Menu Management
   WINDOW* win;
@@ -180,22 +180,16 @@ int a2i(const char *s)
   int maxY, maxX;
   //declaration of functions block
   //append
-  void addBook();
-  void addFurniture();
-  void addComputer();
-  void addOther();
+  void addBook(std::string item_name);
+  void addFurniture(std::string item_name);
+  void addComputer(std::string item_name);
+  void addOther(std::string item_name);
   //get info
-  void getInfoAll();
-  void getInfoBook();
-  void getInfoFurniture();
-  void getInfoComputers();
-  void getInfoOther();
+  void getInfoAll(std::string item_name);
+  void getInfo(std::string item_name);
   //delete
-  void deleteAll();
-  void deleteBook();
-  void deleteFurniture();
-  void deleteComputer();
-  void deleteOther();
+  void deleteAll(std::string item_name);
+  void deleteInfo(std::string item_name);
 
   void init_ncurses() { //general initialization
     initscr();
@@ -298,31 +292,31 @@ int a2i(const char *s)
           case 0: { 
             unpost_menu(current_menu);
             wrefresh(win);
-            addBook(); 
+            addBook("Book"); 
             post_menu(current_menu);
             break;
           }
           case 1: {
             unpost_menu(current_menu);
             wrefresh(win);
-            addFurniture(); 
+            addFurniture("Furniture"); 
             post_menu(current_menu);
             break;
           }
           case 2: {
             unpost_menu(current_menu);
             wrefresh(win);
-            addComputer(); 
+            addComputer("Computer"); 
             post_menu(current_menu);
             break;
           }
-      //    case 3: {
-      //      unpost_menu(current_menu);
-      //      wrefresh(win);
-      //      addOther(); 
-      //      post_menu(current_menu);
-      //      break;
-      //    }
+          case 3: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            addOther("Other"); 
+            post_menu(current_menu);
+            break;
+          }
           case 4: {
             current_menu = menus[0]; 
             break;
@@ -331,41 +325,44 @@ int a2i(const char *s)
       }
       else if(current_menu == menus[2]) { //Get info menu actions
         switch(idx) {
-      //   case 0: {
-      //     unpost_menu(current_menu);
-      //     wrefresh(win);
-      //     getInfoAll(); 
-      //     post_menu(current_menu);
-      //     break;
-      //   }
+         case 0: {
+           unpost_menu(current_menu);
+           wrefresh(win);
+           getInfo("Book");
+           getInfo("Furniture");
+           getInfo("Computer");
+           getInfo("Other");
+           post_menu(current_menu);
+           break;
+         }
           case 1: {
             unpost_menu(current_menu);
             wrefresh(win);
-            getInfoBook(); 
+            getInfo("Book"); 
             post_menu(current_menu);
             break;
           }
-      //    case 2: {
-      //      unpost_menu(current_menu);
-      //      wrefresh(win);
-      //      getInfoFurniture(); 
-      //      post_menu(current_menu);
-      //      break;
-      //      }
-      //    case 3: {
-      //      unpost_menu(current_menu);
-      //      wrefresh(win);
-      //      getInfoComputers(); 
-      //      post_menu(current_menu);
-      //      break;
-      //      }
-      //    case 4: {
-      //      unpost_menu(current_menu);
-      //      wrefresh(win);
-      //      getInfoOther(); 
-      //      post_menu(current_menu);
-      //      break;
-      //      }
+          case 2: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            getInfo("Furniture"); 
+            post_menu(current_menu);
+            break;
+            }
+          case 3: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            getInfo("Computer"); 
+            post_menu(current_menu);
+            break;
+            }
+          case 4: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            getInfo("Other"); 
+            post_menu(current_menu);
+            break;
+            }
           case 5: {
             current_menu = menus[0]; 
             break;
@@ -374,41 +371,34 @@ int a2i(const char *s)
       }
       else if(current_menu == menus[3]) {
         switch(idx) {
-        // case 0: {
-        //   unpost_menu(current_menu);
-        //   wrefresh(win);
-        //   deleteAll(); 
-        //   post_menu(current_menu);
-        //   break;
-        // }
-          case 1: {
+          case 0: {
             unpost_menu(current_menu);
             wrefresh(win);
-            deleteBook(); 
+            deleteInfo("Book"); 
             post_menu(current_menu);
             break;
           }
-        //  case 2: {
-        //    unpost_menu(current_menu);
-        //    wrefresh(win);
-        //    deleteFurniture(); 
-        //    post_menu(current_menu);
-        //    break;
-        //  }
-        //  case 3: {
-        //    unpost_menu(current_menu);
-        //    wrefresh(win);
-        //    deleteComputer(); 
-        //    post_menu(current_menu);
-        //    break;
-        //  }
-        //  case 4: {
-        //    unpost_menu(current_menu);
-        //    wrefresh(win);
-        //    deleteOther(); 
-        //    break;
-        //  }
-          case 5: {
+          case 1: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            deleteInfo("Furniture"); 
+            post_menu(current_menu);
+            break;
+          }
+          case 2: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            deleteInfo("Computer"); 
+            post_menu(current_menu);
+            break;
+          }
+          case 3: {
+            unpost_menu(current_menu);
+            wrefresh(win);
+            deleteInfo("Other"); 
+            break;
+          }
+          case 4: {
             current_menu = menus[0]; 
             break;
           }
@@ -424,29 +414,31 @@ bool is_empty(std::ifstream& pFile)
 {
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
-void addBook() {
+void addBook(std::string item_name) {
   Book book;
   echo();
-  std::ofstream file("books.txt", std::ios_base::app);
+  std::string file_name = (item_name+".txt");
+  std::ofstream file(file_name, std::ios_base::app);
   if (!file.is_open()) {
       mvprintw(maxY/2, maxX/2 - 20, "Error: Unable to open file for writing.");
       return;
   }
-  mvprintw(maxY / 2 - 9, maxX / 2 - 5, "Enter book details:");
-  mvprintw(maxY / 2 - 8, maxX / 2 - 6, "Title: ");
+  mvprintw(maxY / 2 - 9, 1, "Enter book details:");
+  mvprintw(maxY / 2 - 8, 1, "Title: ");
   book.SetName();
-  mvprintw(maxY / 2 - 7, maxX / 2 - 6, "Author: ");
+  mvprintw(maxY / 2 - 7, 1, "Author: ");
   book.SetAuthor();
-  mvprintw(maxY / 2 - 6, maxX / 2 - 6, "ISBN: ");
+  mvprintw(maxY / 2 - 6, 1, "ISBN: ");
   book.SetIsbn();
-  mvprintw(maxY / 2 - 5, maxX / 2 - 6, "ID: ");
-  book.SetId();
-  mvprintw(maxY / 2 - 4, maxX / 2 - 6, "Quantity: ");
+  
+  mvprintw(maxY / 2 - 5, 1, "Quantity: ");
   book.SetQuantity();
+  mvprintw(maxY / 2 - 4, 1, "ID: ");
+  book.SetId();
   for (int i = 0; i < book.GetQuantityINT(); ++i) {
     file << book.GetAuthor() << " - \"" << book.GetName() << "\" ISBN: " << book.GetIsbn() << " ID: " << book.GetId() << "-" << i << "\n";
   }
-  mvprintw(maxY / 2 - 3, maxX / 2 - 6, "Book added successfully.");
+  mvprintw(maxY / 2 - 3, 1, "Book added successfully.");
   getch();
   file.close();
   clear();
@@ -454,27 +446,28 @@ void addBook() {
   wrefresh(win);
   noecho();
 }
-void addFurniture() {
+void addFurniture(std::string item_name) {
   Furniture furn;
   echo();
-  std::ofstream file("furniture.txt", std::ios_base::app);
+  std::string file_name = (item_name+".txt");
+  std::ofstream file(file_name, std::ios_base::app);
   if (!file.is_open()) {
       mvprintw(maxY/2, maxX/2 - 20, "Error: Unable to open file for writing.");
       return;
   }
-  mvprintw(maxY / 2 - 9, maxX / 2 - 10, "Enter furniture element details:");
-  mvprintw(maxY / 2 - 8, maxX / 2 - 6, "Title: ");
+  mvprintw(maxY / 2 - 9, 1, "Enter furniture element details:");
+  mvprintw(maxY / 2 - 8, 1, "Title: ");
   furn.SetName();
-  mvprintw(maxY / 2 - 7, maxX / 2 - 6, "Material: ");
+  mvprintw(maxY / 2 - 7, 1, "Material: ");
   furn.SetMaterial();
-  mvprintw(maxY / 2 - 6, maxX / 2 - 6, "Quantity: ");
+  mvprintw(maxY / 2 - 6, 1, "Quantity: ");
   furn.SetQuantity();
-  mvprintw(maxY / 2 - 5, maxX / 2 - 6, "ID: ");
+  mvprintw(maxY / 2 - 5, 1, "ID: ");
   furn.SetId();
   for (int i = 0; i < furn.GetQuantityINT(); ++i) {
     file << "Element name: " << furn.GetName() << " Material: " << furn.GetMaterial() << " - ID: " << furn.GetId() << "-" << i << "\n";
   }
-  mvprintw(maxY / 2 - 4, maxX / 2 - 14, "Furniture element added successfully.");
+  mvprintw(maxY / 2 - 4, 1, "Furniture element added successfully.");
   getch();
   file.close();
   clear();
@@ -482,34 +475,35 @@ void addFurniture() {
   wrefresh(win);
   noecho();
 }
-void addComputer() {
+void addComputer(std::string item_name) {
   Computer comp;
   echo();
-  std::ofstream file("computers.txt", std::ios_base::app);
+  std::string file_name = (item_name+".txt");
+  std::ofstream file(file_name, std::ios_base::app);
   if (!file.is_open()) {
       mvprintw(maxY/2, maxX/2 - 20, "Error: Unable to open file for writing.");
       return;
   }
-  mvprintw(maxY / 2 - 9, maxX / 2 - 10, "Enter computer details:");
-  mvprintw(maxY / 2 - 8, maxX / 2 - 12, "Enter model name. If you know what you do, you can make something up here");
+  mvprintw(maxY / 2 - 9, 1, "Enter computer details:");
+  mvprintw(maxY / 2 - 8, 1, "Enter model name:");
   comp.SetName();
-  mvprintw(maxY / 2 - 7, maxX / 2 - 12, "Supply info about it's compartments. If there is none - leave derived field empty.");
-  mvprintw(maxY / 2 - 6, maxX / 2 - 12, "Supply CPU info: ");
+  mvprintw(maxY / 2 - 7, 1, "If something is missing - leave derived field empty.");
+  mvprintw(maxY / 2 - 6, 1, "Supply CPU info: ");
   comp.SetCPU();
-  mvprintw(maxY / 2 - 5, maxX / 2 - 12, "Supply GPU info: ");
+  mvprintw(maxY / 2 - 5, 1, "Supply GPU info: ");
   comp.SetGPU();
-  mvprintw(maxY / 2 - 4, maxX / 2 - 12, "Supply RAM info: ");
+  mvprintw(maxY / 2 - 4, 1, "Supply RAM info: ");
   comp.SetRAM();
-  mvprintw(maxY / 2 - 3, maxX / 2 - 12, "Supply Motherboard info: ");
+  mvprintw(maxY / 2 - 3, 1, "Supply Motherboard info: ");
   comp.SetMB();
-  mvprintw(maxY / 2 - 1, maxX / 2 - 12, "Quantity: ");
-  comp.SetQuantity();
-  mvprintw(maxY / 2 - 2, maxX / 2 - 12, "ID: ");
+  mvprintw(maxY / 2 - 2, 1, "ID: ");
   comp.SetId();
+  mvprintw(maxY / 2 - 1, 1, "Quantity: ");
+  comp.SetQuantity();
   for (int i = 0; i < comp.GetQuantityINT(); ++i) {
     file << "Model: " << comp.GetName() << " CPU: " << comp.GetCPU() << " GPU: " << comp.GetGPU() << " RAM: " << comp.GetRAM() << " Motheboard: " << comp.GetMB() << " ID: " << comp.GetId() << "-" << i << "\n";
   }
-  mvprintw(maxY / 2 - 4, maxX / 2 - 14, "Computer added successfully.");
+  mvprintw(maxY / 2, 1, "Computer added successfully.");
   getch();
   file.close();
   clear();
@@ -517,83 +511,133 @@ void addComputer() {
   wrefresh(win);
   noecho();
 }
-void addOther() {
-
-}
-void getInfoBook() {
+void addOther(std::string item_name) {
   echo();
-  std::ifstream file("books.txt");
+  Unregistered other;
+  std::string file_name = (item_name+".txt");
+  std::ofstream file(file_name, std::ios_base::app);
+  if (!file.is_open()) {
+      mvprintw(maxY/2, maxX/2 - 20, "Error: Unable to open file for writing.");
+      return;
+  }
+  mvprintw(maxY / 2 - 9, maxX / 2 - 10, "Enter unregistered item details: ");
+  mvprintw(maxY / 2 - 8, 1, "Title: ");
+  other.SetName();
+  mvprintw(maxY / 2 - 7, 1, "Material: ");
+  other.SetMaterial();
+  mvprintw(maxY / 2 - 6, 1, "Quantity: ");
+  other.SetQuantity();
+  for (int i = 0; i < other.GetQuantityINT(); ++i) {
+    file << "Element name: " << other.GetName() << " Material: " << other.GetMaterial() << " - No" << i << "\n";
+  }
+  mvprintw(maxY / 2 - 4, maxX / 2 - 14, "Unregistered element added successfully.");
+  getch();
+  file.close();
+  clear();
+  werase(win);
+  wrefresh(win);
+  noecho();
+}
+void getInfo(std::string item_name) {
+  //viewPort imitation
+  auto printContent = [&](int start, int end, const std::vector<std::string> content) 
+    {
+      werase(win);
+      for (int i = start; i < start+end && i < content.size(); ++i) {
+        mvprintw(i - start, 0, "%s", content[i].c_str());
+      }
+      wrefresh(win);
+  };
+  noecho();
+  std::string file_name = (item_name+".txt");
+  std::ifstream file(file_name);
+  std::vector<std::string> content;
   if (!file.is_open()) {
       mvprintw(maxY/2, maxX/2 - 19, "Error: Unable to open file for reading.");
       getch();
       return;
   }
   if (is_empty(file)) {
-    mvprintw(maxY/2, (maxX/2 - 11), "There are no books yet");
+    mvprintw(maxY/2, (maxX/2 - 11), "There are no entries yet");
     return;
   }
   clear();
-  mvprintw(maxY / 2 - 5, maxX / 2 - 9, "Books Information:");
   std::string line;
-  int row = maxY/2 - 3; 
+  int startLine = 0;
+  int endLine = LINES - 2;
   while (std::getline(file, line)) {
-    ++row;
-    mvprintw(row, maxX / 2 - 12, line.c_str()); 
+    content.push_back(line);
   }
+  content.push_back("Press enter to close this window");
+  content.push_back("You may navigate with arrows");
   file.close();
-  mvprintw(row + 2, maxX/2 - 41, "Press any key to return to the main menu."); 
-  getch(); 
+  printContent(startLine, endLine, content);
+  int sw;
+  while((sw = getch()) != '\n') {
+    switch (sw) {
+      case KEY_UP: {
+        if (startLine > 0) {
+          --startLine;
+          printContent(startLine, endLine, content);
+        }
+        break;
+      }
+      case KEY_DOWN: {
+        if (startLine + endLine < content.size()) {
+          ++startLine;
+          printContent(startLine, endLine, content);
+        }
+        break;
+      } 
+    }
+  }
   clear();
   werase(win);
   wrefresh(win);
   noecho(); 
 }
-
-void deleteBook() {
+void deleteInfo(std::string item_name) {
   clear();
   echo();
-  std::ifstream inFile("books.txt");
+  std::string file_name = (item_name+".txt");
+  std::ifstream inFile(file_name);
   if (!inFile.is_open()) {
-      mvprintw(0, 0, "Error: Unable to open file for reading.");
+      mvprintw(0, maxX / 2 - 19, "Error: Unable to open file for reading.");
       return;
   }
-
   std::ofstream outFile("temp.txt");
   if (!outFile.is_open()) {
-      mvprintw(0, 0, "Error: Unable to open temporary file.");
+      mvprintw(0, maxX / 2 - 19, "Error: Unable to open temporary file.");
       inFile.close();
       return;
   }
-  mvprintw(2, 2, "Enter the title of the book to delete: ");
-  char title[100];
-  getstr(title);
-
+  mvprintw(2, 2, "Enter ID or title of the items to delete: ");
+  char id[100];
+  getstr(id);
   std::string line;
   bool deleted = false;
   while (std::getline(inFile, line)) {
-      if (line.find(title) == std::string::npos) {
+      if (line.find(id) == std::string::npos) {
           outFile << line << std::endl;
       } else {
           deleted = true;
       }
   }
-
   inFile.close();
   outFile.close();
-  remove("books.txt");
-  rename("temp.txt", "books.txt");
-
+  remove(file_name.c_str());
+  rename("temp.txt", file_name.c_str());
   if (deleted) {
-      mvprintw(4, 2, "Book deleted successfully.");
+      mvprintw(4, 2, ("Item deleted successfully."));
   } else {
-      mvprintw(4, 2, "Book not found.");
+      mvprintw(4, 2, (file_name.c_str(), " not found."));
   }
+  getch();
   clear();
   werase(win);
   wrefresh(win);
   noecho();
 }
-
 int main() {
   navigate();
   return 0;
