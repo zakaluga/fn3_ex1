6d62f058d96630e3753a15ed0e769224b5dcdd99
Mas2rbek <karol057569@gmail.com>
2024-05-21 15:47:28 +0300

    Решены проблемы, добавлены так называемые функции (параметры)

--- main.cpp
+++ main.cpp
@@ -4,19 +4,149 @@
 #include <vector>
 #include <map>
 #include <cstdio>
+bool findItem(std::vector<std::string> vect, std::string name)
+{
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if (name == vect[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
+bool correct(std::string x, int n)
+{
+    std::vector<std::string> right;
+    for (int i = 0; i < n; i++)
+    {
+        right.push_back(std::to_string(i + 1));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (x == right[i])
+        {
+            return true;
+            break;
+        }
+    }
+    return false;
+}
 class AggregatedSettings
 {
 public:
+    int funcscounter;
     std::string name;
     std::string size;
     std::string material;
     std::string extraInf;
     std::string location = "Classroom";
+    std::string number;
+    std::map<std::string, std::string> functions;
+    std::vector<std::string> funcnames;
     void display()
     {
         std::cout << "Item name: " << name << " | Size: "
                   << size << " | Material: " << material << " | Location: " << location
-                  << " | Extra information: " << extraInf << "\n";
+                  << " | Tag: " << number << " | Extra information: " << extraInf << "\n";
+        if (funcscounter != 0)
+        {
+            std::cout << "Functions: \n";
+            for (int i = 0; i < funcscounter; i++)
+            {
+                std::cout << funcnames[i] << ": " << functions[funcnames[i]] << "\n";
+            }
+        }
+    }
+    void getfuncs()
+    {
+        std::string funcname;
+        std::string deletename;
+        std::string changename;
+        std::string funcvalue;
+        int choice;
+        std::string tempchoice;
+        while (choice != 5)
+        {
+            std::cout << "\n";
+            std::cout << "1) Add new function\n";
+            std::cout << "2) Change function value\n";
+            std::cout << "3) Delete concrete function\n";
+            std::cout << "4) Delete all function\n";
+            std::cout << "5) Exit\n";
+            std::getline(std::cin, tempchoice);
+            while (!correct(tempchoice, 5))
+            {
+                std::cout << "Wrong point, try again\n";
+                std::getline(std::cin, tempchoice);
+            }
+            choice = std::stoi(tempchoice);
+            switch (choice)
+            {
+            case 1:
+                funcname.clear();
+                funcvalue.clear();
+                std::cout << "Enter function name: ";
+                std::getline(std::cin, funcname);
+                while (findItem(funcnames, funcname))
+                {
+                    std::cout << "Such a name already exists come up with a new one: ";
+                    std::getline(std::cin, funcname);
+                }
+                std::cout << "Enter function value: ";
+                std::getline(std::cin, funcvalue);
+                functions[funcname] = funcvalue;
+                funcnames.push_back(funcname);
+
+                break;
+            case 2:
+                std::cout << "Which function will be changed: ";
+                std::getline(std::cin, changename);
+                if (findItem(funcnames, changename))
+                {
+                    std::cout << "New function value: ";
+                    std::getline(std::cin, functions[changename]);
+                }
+                else
+                {
+                    std::cout << "Wrong function name\n";
+                    break;
+                }
+                break;
+                break;
+            case 3:
+                if (findItem(funcnames, deletename))
+                {
+                    functions[deletename].clear();
+                    for (auto it = funcnames.begin(); it != funcnames.end(); ++it)
+                    {
+                        if (*it == deletename)
+                        {
+                            funcnames.erase(it);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    std::cout << "Wrong name\n";
+                    break;
+                }
+                break;
+            case 4:
+                functions.clear();
+                funcnames.clear();
+                break;
+            case 5:
+                funcscounter = functions.size();
+                break;
+
+            default:
+                std::cout << "Wrong point, try again\n";
+                break;
+            }
+        }
     }
     void getedinf()
     {
@@ -25,46 +155,65 @@ public:
         std::getline(std::cin, material);
         std::cout << "Size: ";
         std::getline(std::cin, size);
-        std::cout << "Extra information extraInf item: ";
+        std::cout << "Location of item: ";
+        std::getline(std::cin, location);
+        std::cout << "Tag of item: ";
+        std::getline(std::cin, number);
+        std::cout << "Extra information: ";
         std::getline(std::cin, extraInf);
     }
     void getnotedinf()
     {
+
+        std::cin.ignore();
+        std::cout << "Tag of item: ";
+        std::getline(std::cin, number);
         std::cout << "Enter information: ";
         std::getline(std::cin, extraInf);
+        getfuncs();
     }
     void addInToFile(std::ofstream &File)
     {
         File << "Item name: " << name << " | Size: "
-             << size << " | Material: " << material
+             << size << " | Material: " << material << " | Location: " << location << " | Tag: " << number
              << " | Extra information: " << extraInf << "\n";
+        if (funcscounter != 0)
+        {
+            File << "Functions: \n";
+            for (int i = 0; i < funcscounter; i++)
+            {
+                File << funcnames[i] << ": " << functions[funcnames[i]] << "\n";
+            }
+        }
     }
     void addInToTempfile(std::fstream &File)
     {
         File << name << "\n";
         File << size << "\n";
         File << material << "\n";
-        File << extraInf << "\n";
         File << location << "\n";
+        File << number << "\n";
+        File << extraInf << "\n";
+        File << funcscounter << "\n";
+        for (int i = 0; i < funcscounter; i++)
+        {
+            File << funcnames[i] << "\n"
+                 << functions[funcnames[i]] << "\n";
+        }
     }
     void clear()
     {
-        name = "";
-        size = "";
-        material = "";
-        extraInf = "";
+        name.clear();
+        size.clear();
+        material.clear();
+        extraInf.clear();
         location = "Classroom";
+        number.clear();
+        functions.clear();
+        funcnames.clear();
+        funcscounter = 0;
     }
 };
-std::vector<std::string> makenumber(std::string name, int count)
-{
-    std::vector<std::string> temp;
-    for (int i = 0; i < count; i++)
-    {
-        temp.push_back(name + std::to_string(i + 1));
-    }
-    return temp;
-}
 class ClassroomInventory
 {
 private:
@@ -86,60 +235,45 @@ public:
 };
 void scanfile(std::fstream &file, std::map<std::string, AggregatedSettings> &map, std::vector<std::string> &gg)
 {
-    AggregatedSettings temp;
+    std::string line1;
     std::string line;
+    std::string funcname;
+    std::string funcvalue;
     if (file.is_open())
     {
         file.seekg(0);
         while (getline(file, line))
         {
+            AggregatedSettings temp;
             temp.name = line;
-            file.ignore();
             getline(file, temp.size);
             getline(file, temp.material);
-            getline(file, temp.extraInf);
             getline(file, temp.location);
-            map[temp.name] = temp;
+            getline(file, temp.number);
+            getline(file, temp.extraInf);
+            getline(file, line1);
+            temp.funcscounter = std::stoi(line1);
+            for (int i = 0; i < temp.funcscounter; i++)
+            {
+                getline(file, funcname);
+                getline(file, funcvalue);
+                temp.functions[funcname] = funcvalue;
+                temp.funcnames.push_back(funcname);
+            }
+            map[temp.number] = temp;
             if (!temp.name.empty())
             {
-                gg.push_back(temp.name);
-                map[temp.name].display();
+                gg.push_back(temp.number);
+                map[temp.number].display();
             }
         }
     }
     std::cout << "\n";
 }
-bool findItem(std::vector<std::string> vect, std::string name)
-{
-    for (int i = 0; i < vect.size(); i++)
-    {
-        if (name == vect[i])
-        {
-            return true;
-            break;
-        }
-    }
-    return false;
-}
-bool correct(std::string x)
-{
-    std::vector<std::string> right;
-    for (int i = 0; i < 7; i++)
-    {
-        right.push_back(std::to_string(i + 1));
-    }
-    for (int i = 0; i < 7; i++)
-    {
-        if (x == right[i])
-        {
-            return true;
-            break;
-        }
-    }
-    return false;
-}
+
 void Menu(ClassroomInventory &inventory)
 {
+    std::string choice7;
     std::vector<std::string> numbers;
     int tempcount;
     std::string tempname;
@@ -157,7 +291,7 @@ void Menu(ClassroomInventory &inventory)
     scanfile(tempfile, items, names);
     tempfile.close();
     tempfile.open("DON'T_CLICK_HERE.txt", std::ios::out | std::ios::in | std::ios::trunc);
-    while (choice != 7)
+    while (choice != 9)
     {
         std::cout << "\n";
         std::cout << "Menu:\n";
@@ -167,9 +301,11 @@ void Menu(ClassroomInventory &inventory)
         std::cout << "4) Delete all items.\n";
         std::cout << "5) Delete choosen item\n";
         std::cout << "6) Display all items\n";
-        std::cout << "7) Exit.\n";
+        std::cout << "7) Change functions\n";
+        std::cout << "8) Save in to file\n";
+        std::cout << "9) Exit.\n";
         std::getline(std::cin, tempchoice);
-        while (!correct(tempchoice))
+        while (!correct(tempchoice, 9))
         {
             std::cout << "Wrong point, try again\n";
             std::getline(std::cin, tempchoice);
@@ -182,19 +318,12 @@ void Menu(ClassroomInventory &inventory)
             std::cin >> tempname;
             std::cout << "Enter number of items: ";
             std::cin >> tempcount;
-            numbers = makenumber(tempname, tempcount);
-            while (findItem(names, numbers[0]))
-            {
-                std::cout << "Such a name has already been found, come up with a new one\n";
-                std::cin >> tempname;
-                numbers = makenumber(tempname, tempcount);
-            }
-            temp.getedinf();
             for (int i = 0; i < tempcount; i++)
             {
-                temp.name = numbers[i];
-                items[temp.name] = temp;
-                names.push_back(temp.name);
+                temp.name = tempname;
+                temp.getedinf();
+                items[temp.number] = temp;
+                names.push_back(temp.number);
             }
             temp.clear();
             break;
@@ -203,19 +332,12 @@ void Menu(ClassroomInventory &inventory)
             std::cin >> tempname;
             std::cout << "Enter number of items: ";
             std::cin >> tempcount;
-            numbers = makenumber(tempname, tempcount);
-            while (findItem(names, numbers[0]))
+            for (int i = 0; i < tempcount; i++)
             {
-                std::cout << "Such a name has already been found, come up with a new one\n";
-                std::cin >> tempname;
-                numbers = makenumber(tempname, tempcount);
-            }
-            temp.getnotedinf();
-            for (int i; i < tempcount; i++)
-            {
-                temp.name = numbers[i];
-                items[temp.name] = temp;
-                names.push_back(temp.name);
+                temp.name = tempname;
+                temp.getnotedinf();
+                items[temp.number] = temp;
+                names.push_back(temp.number);
             }
             temp.clear();
             break;
@@ -230,7 +352,7 @@ void Menu(ClassroomInventory &inventory)
                 std::cout << "3) Extra information\n";
                 std::cout << "4) Exit\n";
                 std::getline(std::cin, tempchoice);
-                while (!correct(tempchoice))
+                while (!correct(tempchoice, 4))
                 {
                     std::cout << "Wrong point, try again\n";
                     std::getline(std::cin, tempchoice);
@@ -263,10 +385,6 @@ void Menu(ClassroomInventory &inventory)
             break;
 
         case 4:
-            for (int i = 0; i < names.size(); i++)
-            {
-                std::remove((names[i] + ".txt").c_str());
-            }
             file.clear();
             tempfile.clear();
             items.clear();
@@ -300,6 +418,19 @@ void Menu(ClassroomInventory &inventory)
             }
             break;
         case 7:
+            std::cout << "Which item will be changed: ";
+            std::getline(std::cin, choice7);
+            if (findItem(names, choice7))
+            {
+                items[choice7].getfuncs();
+            }
+            else
+            {
+                std::cout << "Wrong function name\n";
+                break;
+            }
+            break;
+        case 8:
             for (int i = 0; i < names.size(); i++)
             {
                 inventory.addItems(items[names[i]]);
@@ -307,6 +438,8 @@ void Menu(ClassroomInventory &inventory)
             inventory.addInToFile(file, tempfile);
             file.close();
             tempfile.close();
+            break;
+        case 9:
             break;
         }
     }
