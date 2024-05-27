bcce6cdcfd789104a41f8db751377c01b7db715b
JonhNash <igor.study495@gmail.com>
2024-05-27 14:47:26 +0300

    release methods: row, col, setItem.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 
-using MASSIVE = /*двумерный массив(вектор)*/};
-using VECTOR = /*Одномерный массив(вектор)*/};
+using MASSIVE = std::vector<std::vector<double>>;
+using VECTOR = std::vector<double>;
 
 class MATRIX {
     // Матрица организована как вектор строк(row),
     // т.е. сначала идет строка, потом обращение к колонке
 
 protected:
-    //Данные о матрице
-
+    MASSIVE matr;
 public:
     MATRIX(int rows, int columns);
     MATRIX(std::initializer_list<std::initializer_list<double>> matr);
@@ -22,7 +23,7 @@ public:
     int    dim() const;
     void   resize(int newRow, int newCol, bool isSafeResize = true);
 
-    string toString() const;
+    std::string toString() const;
     void   print();
 
     void   setItem(int row, int columns, double value);
@@ -33,7 +34,7 @@ public:
     VECTOR row(int indx);
     VECTOR col(int indx);
 
-    const MASSIVE& matr() const{return data;} // для константных объектов
+    //const MASSIVE& matr() const{return data;} // для константных объектов
 
     double  det();               // Вычисление определителя
     MATRIX T();                  // Транспонированная матрица
@@ -71,6 +72,39 @@ public:
     friend std::ostream & operator<<(std::ostream &, const MATRIX &);
 };
 
+MATRIX operator*(const MATRIX& Matr1, const MATRIX& Matr2)
+{
+    if ((Matr1.colLen() & Matr2.rowLen()) != Matr1.rowLen()) {
+        throw std::string("Кол-во столбцов первой матрицы не равно кол-ву строк второй - умножение невозможно ");
+    }    
+    MATRIX ResultMatrix(Matr1.rowLen(), Matr2.colLen());
+    for(int i = 0; i < Matr1.colLen(); ++i)
+    {
+        for(int j = 0; j < Matr2.rowLen(); ++j)
+        {
+
+        }
+    }
+
+}
+
+VECTOR MATRIX::col(int ind)
+{
+    VECTOR ResultVector(matr.size()); // размер столбца
+    for(int col = 0; col < matr.size(); ++col)
+    {
+        ResultVector[col] = matr[col][ind];
+    }
+    return ResultVector;
+}
+VECTOR MATRIX::row(int ind)
+{
+    return VECTOR(matr[ind]);
+}
+void MATRIX::setItem(int row, int col, double value)
+{
+    matr[row][col] = value;
+}
 
 int main(){
     std::cout << "Hello, world!" << std::endl;
